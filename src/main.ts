4bd5d5f5279a7e74b3a7295d7cbf0a1cb4cdb555
jinkwan.kim <kkole3897@gmail.com>
2022-02-19 15:39:23 +0900

    chore: init project

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import PortalVue from "portal-vue";
+import VueI18n from "vue-i18n";
 
 Vue.config.productionTip = false;
+
+Vue.use(PortalVue);
+Vue.use(VueI18n);
 
 new Vue({
   router,
